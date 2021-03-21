d5a709872508e8c12dcd0f5b210d926944987f0b
Matt Manzi <imyourmanzi1@gmail.com>
2021-03-21 17:29:59 -0400

    add support to plant terminal perennial

--- perennials/terminal/provided.sh
+++ perennials/terminal/provided.sh
@@ -3,6 +3,8 @@
 # Author: Matt Manzi
 # Date: 2021-02-16
 #
+# macOS wants to deal with plists only and I'm feeling lazy.
+#
 # Prerequisites:
 # - FIG_HOME is set to the Fig Tree project directory.
 
@@ -14,4 +16,20 @@ if [[ -z "${FIG_HOME+x}" ]]; then
     exit 1
 fi
 
-# INSTALL_TARGET=
+#### Includes
+source "${FIG_HOME}/tool-shed/logger.bash"
+
+
+#### Globals
+__PERENNIAL_DIR="perennials/ssh"
+__SOURCE_FILE="${FIG_HOME}/${__PERENNIAL_DIR}/Fig Tree Dark.terminal"
+
+
+#### Main Script
+
+logDebug "Planting perennial: ${__PERENNIAL_DIR}"
+logTrace "Source file: ${__SOURCE_FILE}"
+logInfo "Terminal will launch shortly (if it is not running already) and a new window will open.  It can be closed at any time."
+sleep 1
+
+open "${__SOURCE_FILE}"
