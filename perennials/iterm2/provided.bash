3029ab1fb739ab0a6cfa7c715efbf966cfe7f32b
Matt Manzi <imyourmanzi1@gmail.com>
2021-03-21 15:25:48 -0400

    add planting for iterm2 perennial

--- perennials/iterm2/provided.bash
+++ perennials/iterm2/provided.bash
@@ -2,15 +2,35 @@
 # Author: Matt Manzi
 # Date: 2021-02-16
 #
+# Utilizes iTerm2's Dynamic Profiles feature:
+# https://iterm2.com/documentation-dynamic-profiles.html
+#
 # Prerequisites:
 # - FIG_HOME is set to the Fig Tree project directory.
 
 if [[ -z "${FIG_HOME+x}" ]]; then
     echo -ne "\033[38;5;160m"
-    echo -e "[FATL] FIG_HOME variable not set!  Check that is being defined."
+    echo -e "[FATL] FIG_HOME variable not set!  Check that it is being defined."
     echo -e "\033[0m"
     read -p "Shell will exit on <return>"
     exit 1
 fi
 
-# INSTALL_TARGET=
+#### Includes
+source "${FIG_HOME}/tool-shed/logger.bash"
+source "${FIG_HOME}/tool-shed/linker.bash"
+
+
+#### Globals
+__INSTALL_TARGET="${HOME}/Library/Application Support/iTerm2/DynamicProfiles/fig_tree.json"
+__PERENNIAL_DIR="perennials/iterm2"
+__SOURCE_FILE="${FIG_HOME}/${__PERENNIAL_DIR}/provided.json"
+
+
+#### Main Script
+
+logDebug "Planting perennial: ${__PERENNIAL_DIR}"
+logTrace "Install target: ${__INSTALL_TARGET}"
+logTrace "Source file: ${__SOURCE_FILE}"
+
+linkSafely
