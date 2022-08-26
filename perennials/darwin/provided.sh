ab010e0b7cb5b46c04f36091cfcbad290415c863
Matt Manzi <imyourmanzi1@gmail.com>
2022-08-26 19:38:58 -0400

    Add OS check for darwin perennial

--- perennials/darwin/provided.sh
+++ perennials/darwin/provided.sh
@@ -17,18 +17,23 @@ fi
 #### Includes
 source "${FIG_HOME}/tool-shed/logger.bash"
 source "${FIG_HOME}/tool-shed/linker.bash"
+source "${FIG_HOME}/tool-shed/globals.bash"
+
+# only run 
+if [[ "${FIG_OS}" == "${__FIG_MAC_OS}" ]]; then
+    #### Globals
+    __INSTALL_TARGET="${HOME}/.bash_profile"
+    __PERENNIAL_DIR="perennials/darwin"
+    __SOURCE_FILE="${FIG_HOME}/${__PERENNIAL_DIR}/provided.bash_profile"
 
 
-#### Globals
-__INSTALL_TARGET="${HOME}/.bash_profile"
-__PERENNIAL_DIR="perennials/darwin"
-__SOURCE_FILE="${FIG_HOME}/${__PERENNIAL_DIR}/provided.bash_profile"
+    #### Main Script
 
+    logDebug "Planting perennial: ${__PERENNIAL_DIR}"
+    logTrace "Install target: ${__INSTALL_TARGET}"
+    logTrace "Source file: ${__SOURCE_FILE}"
 
-#### Main Script
-
-logDebug "Planting perennial: ${__PERENNIAL_DIR}"
-logTrace "Install target: ${__INSTALL_TARGET}"
-logTrace "Source file: ${__SOURCE_FILE}"
-
-linkSafely
+    linkSafely
+else
+    logInfo "Not planting darwin, current system is ${FIG_OS}"
+fi
