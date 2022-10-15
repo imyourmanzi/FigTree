58a2e635870fb769eb33f9eca434b6675f27c02e
Matt Manzi <imyourmanzi1@gmail.com>
2022-10-14 23:02:34 -0400

    Remove progress bars and fix font links

--- perennials/fonts/provided.sh
+++ perennials/fonts/provided.sh
@@ -34,28 +34,28 @@ if type brew &>/dev/null; then
 elif [[ "$FIG_OS" == "$__FIG_MAC_OS" ]]; then
     logInfo "Installing Roboto Mono Nerd Font manually"
     cd ~/Library/Fonts
-    curl -fLo "RobotoMono Nerd Font Regular Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Bold Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Bold/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Italic/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Medium Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Medium/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Thin Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Thin/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Light Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Light/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Bold-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Bold-Italic/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Light-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Light-Italic/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Medium-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Medium-Italic/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Thin-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Thin-Italic/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Regular Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Bold Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Bold/complete/Roboto%20Mono%20Bold%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Italic/complete/Roboto%20Mono%20Italic%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Light Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Light/complete/Roboto%20Mono%20Light%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Medium Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Medium/complete/Roboto%20Mono%20Medium%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Thin Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Thin/complete/Roboto%20Mono%20Thin%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Bold-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Bold-Italic/complete/Roboto%20Mono%20Bold%20Italic%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Light-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Light-Italic/complete/Roboto%20Mono%20Light%20Italic%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Medium-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Medium-Italic/complete/Roboto%20Mono%20Medium%20Italic%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Thin-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Thin-Italic/complete/Roboto%20Mono%20Thin%20Italic%20Nerd%20Font%20Complete.ttf
 elif [[ "$FIG_OS" == "$__FIG_LINUX" ]]; then
     logInfo "Installing Roboto Mono Nerd Font manually"
     mkdir -p ~/.local/share/fonts
     cd ~/.local/share/fonts
-    curl -fLo "RobotoMono Nerd Font Regular Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Bold Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Bold/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Italic/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Medium Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Medium/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Thin Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Thin/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Light Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Light/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Bold-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Bold-Italic/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Light-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Light-Italic/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Medium-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Medium-Italic/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
-    curl -fLo "RobotoMono Nerd Font Thin-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Thin-Italic/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Regular Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Bold Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Bold/complete/Roboto%20Mono%20Bold%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Italic/complete/Roboto%20Mono%20Italic%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Light Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Light/complete/Roboto%20Mono%20Light%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Medium Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Medium/complete/Roboto%20Mono%20Medium%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Thin Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Thin/complete/Roboto%20Mono%20Thin%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Bold-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Bold-Italic/complete/Roboto%20Mono%20Bold%20Italic%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Light-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Light-Italic/complete/Roboto%20Mono%20Light%20Italic%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Medium-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Medium-Italic/complete/Roboto%20Mono%20Medium%20Italic%20Nerd%20Font%20Complete.ttf
+    curl -sfLo "RobotoMono Nerd Font Thin-Italic Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Thin-Italic/complete/Roboto%20Mono%20Thin%20Italic%20Nerd%20Font%20Complete.ttf
 fi
